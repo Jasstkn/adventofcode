30edbed9e1a8e98b055419e59a6592b999ce8d1d
Mariia Kotliarevskaia <jasstkn.051@gmail.com>
2023-12-01 16:38:04 +0100

    2023: day01, 2st task
    Signed-off-by: Mariia Kotliarevskaia <jasstkn.051@gmail.com>

--- aoc2023/day01/main.go
+++ aoc2023/day01/main.go
@@ -8,9 +8,10 @@ import (
 	"strings"
 )
 
+const NUMS_DICT = "123456789"
+
 func main() {
 	calibrationSum := 0
-	numsDict := "0123456789"
 
 	lines, err := readLinesFromFile("input.txt")
 	if err != nil {
@@ -23,16 +24,18 @@ func main() {
 		if line == "" {
 			continue
 		}
+		fmt.Println(line)
+		line = replaceNumbers(line)
 
 		i, j := 0, len(line)-1
 		n1, n2 := "", ""
 		for i <= j && (n1 == "" || n2 == "") {
-			if strings.Contains(numsDict, string(line[i])) {
+			if strings.Contains(NUMS_DICT, string(line[i])) {
 				n1 = string(line[i])
 			} else {
 				i++
 			}
-			if strings.Contains(numsDict, string(line[j])) {
+			if strings.Contains(NUMS_DICT, string(line[j])) {
 				n2 = string(line[j])
 			} else {
 				j--
@@ -44,7 +47,7 @@ func main() {
 		if err != nil {
 			slog.Error(line)
 		}
-
+		fmt.Println(calibrationVal)
 		calibrationSum += calibrationVal
 	}
 
@@ -69,4 +72,42 @@ func checkEmptyNumbers(n1, n2 string) (string, string) {
 		return n1, n1
 	}
 	return n1, n2
+}
+
+func replaceNumbers(line string) string {
+	numsWords := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+
+	i, j := 0, 0
+	line += " "
+	newLine := ""
+	for i <= len(line)-1 && j <= len(line)-1 {
+		for k, v := range numsWords {
+			if strings.Contains(line[i:j+1], k) {
+				newLine = newLine + v
+				i = j
+			}
+			if strings.Contains(NUMS_DICT, string(line[i])) {
+				newLine = newLine + string(line[i])
+				i++
+			}
+			if strings.Contains(NUMS_DICT, string(line[j])) {
+				newLine = newLine + string(line[j])
+				i = j + 1
+				j++
+			}
+		}
+		j++
+	}
+
+	return newLine
 }
